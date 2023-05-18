d0e435607bb33a0f0377faf35097a4dac742ea53
jiedjoshua <118013854+jiedjoshua@users.noreply.github.com>
2023-05-18 23:23:16 +0800

    Add files via upload

---
+++ .github/workflows/javenbot.py
@@ -0,0 +1,23 @@
+import schedule
+import time
+from telegram import Bot
+import asyncio
+
+# Telegram Bot API token
+bot_token = '6196088902:AAHxIuetBvYCnweKh5ZfR41WrmQxxiU3NyI'
+
+# User ID to send the reminder
+user_id = '971915251'
+
+# Function to send the reminder message
+async def send_lunch_reminder():
+    bot = Bot(token=bot_token)
+    await bot.send_message(chat_id=user_id, text='It\'s lunchtime! Don\'t forget to eat pretty! 🍽️')
+
+# Schedule the reminder to run at 12 PM every day
+schedule.every().day.at('22:48').do(asyncio.run, send_lunch_reminder())
+
+# Start the scheduler
+while True:
+    schedule.run_pending()
+    time.sleep(1)
